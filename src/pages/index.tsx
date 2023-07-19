0f36732e96b9e56212941b606475d547fb37b981
Samuel Nicholls <samuel.nicholls@hey.com>
2023-07-19 09:37:41 +0100

    Basic trivia card structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,23 @@ const Home: NextPage = () => {
   return (
     <div className="container mx-auto max-w-4xl">
       <Title text="Trivia" />
-      <div className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-        {/* <p>TIMER BAR HERE</p>
-        <p>3 of 10</p>
-        <p className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <div>
-          <div>Lorem ipsum dolor sit amet</div>
-          <div>Lorem ipsum dolor sit amet</div>
-          <div>Lorem ipsum dolor sit amet</div>
-          <div>Lorem ipsum dolor sit amet</div>
-        </div> */}
+      <div className="block p-8 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+          <div className="bg-blue-600 h-2.5 rounded-full" style={{width: "45%"}}></div>
+        </div>
+        <p className="mt-4 text-xl font-bold text-white">3 out of 10</p>
+        <p className='mt-4 text-2xl tracking-tight text-gray-900 dark:text-white text-left'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8">
+          <div className='p-4 border border-white rounded-lg text-white'>Lorem ipsum dolor sit amet</div>
+          <div className='p-4 border border-white rounded-lg text-white'>Lorem ipsum dolor sit amet</div>
+          <div className='p-4 border border-white rounded-lg text-white'>Lorem ipsum dolor sit amet</div>
+          <div className='p-4 border border-white rounded-lg text-white'>Lorem ipsum dolor sit amet</div>
+        </div>
+        <div className='mt-8 text-right'>
+        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Next</button>
+        </div>
+      </div>
+      <div>
       </div>
     </div>
   );
